b8ccc0d37a3a2f34ef300653e15498c8736f5469
Moritz Esser <moritz_esser@hotmail.de>
2020-02-06 11:23:05 +0100

    gitignore update and imgetter

--- first.py
+++ first.py
@@ -2,3 +2,12 @@
 # -*- coding: utf8 -*-
 
 __version__ = "0.1"
+
+import os, sys, json, csv
+import requests
+from PIL import Image
+from io import BytesIO
+
+def init():
+    return False
+    return True
